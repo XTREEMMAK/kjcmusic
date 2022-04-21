59f009cf4ca873ef75dcdffec94149d8e3712248
XTREEMMAK <get@jamaale.live>
2022-04-21 11:13:22 -0400

    Move MusicDB location
    Moved the MusicDB location in the js folder since that is pretty much its own Javascript app. Also asjusted some of the file paths for the MusicDB as well

---
+++ public/js/music_db/kjmdb.js
@@ -0,0 +1,110 @@
+const HOME = '/public/js/music_db/'
+const ALBUMS_HOME = HOME+'albums/';
+function getRecordsList (templateFile, recordsJSONFile,sortContainerID){
+    setTemplate (templateFile,true)
+    .then((template) => {
+        setRecordsList (recordsJSONFile,template,sortContainerID)
+    })
+    .catch((err) => {
+      console.log("promise rejected", err);
+    });
+}
+
+function getRecordInfo (templateFile,key){
+    setTemplate (templateFile,false)
+    .then((template) => {
+        setRecordsInfo (key,template)
+    })
+    .catch((err) => {
+      console.log("promise rejected", err);
+    });
+}
+function setTemplate (templateFile,mixerDestroy){
+    return new Promise((resolve,reject) =>{
+    var Request = new XMLHttpRequest();
+    Request.open('GET',templateFile);
+
+    Request.onload = function(){
+        
+        if (Request.status >= 200 && Request.status < 400) {
+            let data = Request.responseText;
+            if (typeof mixer !== 'undefined' || mixerDestroy == true) {
+                mixer.destroy();
+             }
+            resolve(data);
+        }
+        else
+        {
+            reject("We connected to the server, but it an error was returned.");
+        }
+    
+    };
+
+    Request.onerror = function(){
+        console.log("Connection Error Charlie!")
+    };
+
+    Request.send();
+});
+}
+function setRecordsList (listJSON,recordTemplate,mixContainer){
+    var Request = new XMLHttpRequest();
+    Request.open('GET',listJSON);
+    //
+    Request.onload = function(){
+        if (Request.status >= 200 && Request.status < 400) {
+        var inventory = JSON.parse(Request.responseText);
+    
+        //var rawTemplate = document.getElementById(recordTemplate).innerHTML;
+        var compiledTemplate = Handlebars.compile(recordTemplate);
+        var ourGeneratedHTML = compiledTemplate(inventory);
+        var musicLibraryContainer = document.getElementById(mixContainer);
+        musicLibraryContainer.innerHTML = ourGeneratedHTML;
+        var mixer = mixitup('#'+mixContainer);
+        }
+        else
+        {
+            console.log("We connected to the server, but it an error was returned.")
+        }
+    };
+
+    Request.onerror = function(){
+        console.log("Connection Error Charlie!")
+    };
+
+    Request.send();
+}
+
+
+function setRecordsInfo (key,recordTemplate) {
+    
+    var Request = new XMLHttpRequest();
+    Request.open('GET',ALBUMS_HOME+key+'/info.json');
+    //
+    Request.onload = function(){
+        if (Request.status >= 200 && Request.status < 400) {
+        var inventory = JSON.parse(Request.responseText);
+    
+        //var rawTemplate = document.getElementById(recordTemplate).innerHTML;
+        var compiledTemplate = Handlebars.compile(recordTemplate);
+        var ourGeneratedHTML = compiledTemplate(inventory);
+        //var musicLibraryContainer = document.getElementById(mixContainer);
+        modal.setContent(
+            '<div class="record_wrapper">'+
+            '<div class="ri_cover"><img src="'+ALBUMS_HOME+key+'/front.jpg" loading="lazy"/></div>'+ourGeneratedHTML+
+            '</div>');
+        modal.open();
+        //musicLibraryContainer.innerHTML = ourGeneratedHTML;
+        }
+        else
+        {
+            console.log("We connected to the server, but it an error was returned.")
+        }
+    };
+
+    Request.onerror = function(){
+        console.log("Connection Error Charlie!")
+    };
+
+    Request.send();
+}
