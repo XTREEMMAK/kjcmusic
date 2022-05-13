df0c020c57e9eb531e1bc3d191325072c2127228
XTREEMMAK <get@jamaale.live>
2022-05-13 16:57:24 -0400

    Refined README

--- index.php
+++ index.php
@@ -26,9 +26,8 @@
         <link href="<? echo $baseURL; ?>public/css/style.css" rel="stylesheet"/> 
         <link href="<? echo $baseURL; ?>public/js/mp3-sticky-player/content/global.css" rel="stylesheet" type="text/css"/> 
         <link href="<? echo $baseURL; ?>public/js/tingle/tingle.css" rel="stylesheet"/>
-        <link rel="stylesheet" href="https://unpkg.com/driver.js/dist/driver.min.css"/>
         <script>/* Pinegrow Interactions, do not remove */ (function(){try{if(!document.documentElement.hasAttribute('data-pg-ia-disabled')) { window.pgia_small_mq=typeof pgia_small_mq=='string'?pgia_small_mq:'(max-width:767px)';window.pgia_large_mq=typeof pgia_large_mq=='string'?pgia_large_mq:'(min-width:768px)';var style = document.createElement('style');var pgcss='html:not(.pg-ia-no-preview) [data-pg-ia-hide=""] {opacity:0;visibility:hidden;}html:not(.pg-ia-no-preview) [data-pg-ia-show=""] {opacity:1;visibility:visible;display:block;}';if(document.documentElement.hasAttribute('data-pg-id') && document.documentElement.hasAttribute('data-pg-mobile')) {pgia_small_mq='(min-width:0)';pgia_large_mq='(min-width:99999px)'} pgcss+='@media ' + pgia_small_mq + '{ html:not(.pg-ia-no-preview) [data-pg-ia-hide="mobile"] {opacity:0;visibility:hidden;}html:not(.pg-ia-no-preview) [data-pg-ia-show="mobile"] {opacity:1;visibility:visible;display:block;}}';pgcss+='@media ' + pgia_large_mq + '{html:not(.pg-ia-no-preview) [data-pg-ia-hide="desktop"] {opacity:0;visibility:hidden;}html:not(.pg-ia-no-preview) [data-pg-ia-show="desktop"] {opacity:1;visibility:visible;display:block;}}';style.innerHTML=pgcss;document.querySelector('head').appendChild(style);}}catch(e){console&&console.log(e);}})()</script>         
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poiret+One&display=swap"/><link rel="icon" href="public/img/kj_logo_icon.svg" type="image/svg+xml"/></head>     
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poiret+One&display=swap"/><link rel="icon" href="public/img/kj_logo_icon.svg" type="image/svg+xml"/></head>     
     <body> 
         <div class="super_nav"> 
             <div class="desktop_nav"> 
@@ -208,7 +207,6 @@
         <!-- ####### -->
         <div id="blackout" data-pg-ia-hide data-pg-ia='{"l":[{"trg":"click","a":{"l":[{"t":"","l":[{"t":"set","p":0,"d":0,"l":{"pgCall":{"complete":"enableScroll"}}},{"t":"tween","p":0.01,"d":0.5,"l":{"autoAlpha":0}}]},{"t":"$#mplayer_info_box_wrapper","l":[{"t":"tween","p":0,"d":0.5,"l":{"x":"50vw"}}]}]}}]}'></div>          
         <script src="https://use.fontawesome.com/releases/v5.13.1/js/all.js"></script>
-        <script src="https://unpkg.com/driver.js/dist/driver.min.js"></script>
         <script src="<? echo $baseURL; ?>public/js/mp3-sticky-player/java/FWDMSP.js" type="text/javascript"></script>         
         <script src="<? echo $baseURL; ?>public/js/mp3-sticky-player/player_settings.js"></script>         
         <script src="<? echo $baseURL; ?>public/js/tingle/tingle.js"></script>         
